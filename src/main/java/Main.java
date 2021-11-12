5900327bcf472ad866988bcb0883cdb0cab8cddf
pickslayer17 <dzianis.piatrovich@gmail.com>
2021-11-12 15:40:29 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static boolean checkIfInputIsAnEvenNumber(int number){
+        return number % 2 == 0;
+    }
+}
